bd4615b6abf7305f6258f70295d8bceebfe93562
jjasmin123 <43469860+jjasmin123@users.noreply.github.com>
2023-09-13 16:07:22 +0530

    Update LlamaIndex-model1.py

--- LlamaIndex-model1.py
+++ LlamaIndex-model1.py
@@ -1,1 +1,30 @@
+# For OpenAI
 
+import os
+
+os.environ["OPENAI_API_KEY"] = "sk-..."
+
+import logging
+import sys
+
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO
+)  # logging.DEBUG for more verbose output
+
+from llama_index import (
+    KnowledgeGraphIndex,
+    LLMPredictor,
+    ServiceContext,
+    SimpleDirectoryReader,
+)
+from llama_index.storage.storage_context import StorageContext
+from llama_index.graph_stores import NebulaGraphStore
+from llama_index.llms import OpenAI
+
+from IPython.display import Markdown, display
+
+
+# define LLM
+# NOTE: at the time of demo, text-davinci-002 did not have rate-limit errors
+llm = OpenAI(temperature=0, model="text-davinci-002")
+service_context = ServiceContext.from_defaults(llm=llm, chunk_size_limit=512)
